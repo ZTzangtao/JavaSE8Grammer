93953c7288de4ccc6793651515a6ba71351875a5
zt <906577196@qq.com>
2019-05-20 12:00:02 +0800

    集成mybatis-plus

--- src/main/java/lambadaTest/Lambda.java
+++ src/main/java/lambadaTest/Lambda.java
@@ -1,9 +1,7 @@
 package lambadaTest;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author zangtao
@@ -11,8 +9,7 @@ import java.util.Map;
 public class Lambda {
 
     public static void main(String[] args) {
-        String[] atp = {"Rafael Nadal", "Novak Djokovic", "Stanislas Wawrinka", "David Ferrer",
-                             "Roger Federer", "Andy Murray","Tomas Berdych", "Juan Martin Del Potro"};
+        String[] atp = {"Rafael", "Novak", "Stanislas", "David", "Roger", "Andy","Tomas", "Juan"};
         List<String> players =  Arrays.asList(atp);
 
         // 增强for循环
@@ -21,7 +18,7 @@ public class Lambda {
         }
 
         // 使用 lambda 表达式以及函数操作(functional operation)
-        players.forEach((player) -> System.out.print(player + "; "));
+        players.forEach((player) -> System.out.println(player + "; "));
 
         // 在 Java 8 中使用双冒号操作符(double colon operator)
         players.forEach(System.out::println);
