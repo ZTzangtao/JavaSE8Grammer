b6d0406e556a0daff859714a3df65e9a3f4a3da7
zt <906577196@qq.com>
2019-08-08 18:33:27 +0800

    修改

---
+++ src/main/java/classicalAlgorithm/BubbleSort.java
@@ -0,0 +1,38 @@
+package classicalAlgorithm;
+
+/**
+ * 冒泡排序
+ *
+ * @author zangtao
+ * @create 2019 - 08 -08 18:04
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{3, 7, 9, 10, 11, 2};
+
+        int[] b = Sort(a);
+
+        for (int i : b) {
+            System.out.print(i + ",");
+        }
+
+    }
+
+    private static int[] Sort(int[] a) {
+        int t;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                //第二层J判断
+                if (a[j] > a[j + 1]) {
+                    t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+            }
+        }
+
+        return a;
+    }
+
+}
