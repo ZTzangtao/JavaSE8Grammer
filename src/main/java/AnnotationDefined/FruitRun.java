a5c99b3925e40c4141bef0351763ac12be3a8bde
zt <906577196@qq.com>
2019-08-21 19:08:12 +0800

    修改

---
+++ src/main/java/AnnotationDefined/FruitRun.java
@@ -0,0 +1,13 @@
+package AnnotationDefined;
+
+/**
+ * 输出结果
+ *
+ * @author zangtao
+ * @create 2019 - 08 -21 19:05
+ */
+public class FruitRun {
+    public static void main(String[] args) {
+        FruitInfoUtil.getFruitInfo(Apple.class);
+    }
+}
