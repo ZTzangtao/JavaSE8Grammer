48122f1a71f6f17def578e9f3775aeab16bc7cf2
zt <906577196@qq.com>
2019-09-06 17:10:08 +0800

    test

---
+++ src/main/java/OwnTest/StringTest.java
@@ -0,0 +1,20 @@
+package OwnTest;
+
+/**
+ * 字符串练习
+ *
+ * @author zangtao
+ * @create 2019 - 09 -06 17:07
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        String str1 = "string" + 1 + 1;
+
+        String str2 = 1 + 1 + "string";
+
+        System.out.println(str1 + str2);
+    }
+
+
+}
