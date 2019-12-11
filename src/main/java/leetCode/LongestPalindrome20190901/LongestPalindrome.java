265bacb95a5f4c39e1c78edc7e18472b1d7dfb10
Administrator <zangtao-sj@bestpay.com.cn>
2019-12-11 17:29:05 +0800

    回文字符串

--- src/main/java/leetCode/LongestPalindrome20190901/LongestPalindrome.java
+++ src/main/java/leetCode/LongestPalindrome20190901/LongestPalindrome.java
@@ -13,12 +13,19 @@ public class LongestPalindrome {
         System.out.println(longestPalindrome("abajkdfhgsjkdfhbcbcbgiudfhgisdhf"));
     }
 
+    /**
+     * 暴力解法
+     *
+     * @param s
+     * @return
+     */
     public static String longestPalindrome(String s) {
         String ans = "";
         int max = 0;
         int len = s.length();
         String test;
         for (int i = 0; i < len; i++) {
+            //挨个遍历
             for (int j = i + 1; j <= len; j++) {
                 test = s.substring(i, j);
                 if (isPalindromic(test) && test.length() > max) {
@@ -31,6 +38,12 @@ public class LongestPalindrome {
 
     }
 
+    /**
+     * 判断字符串是否是回文
+     *
+     * @param s
+     * @return
+     */
     private static boolean isPalindromic(String s) {
         int len = s.length();
         for (int i = 0; i < len / 2; i++) {
---
+++ src/main/java/leetCode/LongestPalindrome20190901/LongestPalindrome20191211.java
@@ -0,0 +1,23 @@
+package leetCode.LongestPalindrome20190901;
+
+
+/**
+ * 回文最长字符串（动态规划）
+ *
+ * @author zangtao
+ * @date 2019/12/11 8:24
+ */
+public class LongestPalindrome20191211 {
+
+    public String longestPalindrome(String s) {
+        if ("".equals(s)){
+            return "";
+        }
+        String origin = s;
+        String reverse = new StringBuffer(s).reverse().toString();
+
+        return null;
+    }
+
+
+}
