1eeb28925371cea996e4d09c46a40ba36e239ba3
Administrator <zangtao-sj@bestpay.com.cn>
2019-12-12 17:47:14 +0800

    回文字符串

--- src/main/java/leetCode/LongestPalindrome20190901/LongestPalindrome20191211.java
+++ src/main/java/leetCode/LongestPalindrome20190901/LongestPalindrome20191211.java
@@ -9,14 +9,38 @@ package leetCode.LongestPalindrome20190901;
  */
 public class LongestPalindrome20191211 {
 
-    public String longestPalindrome(String s) {
+    public static String longestPalindrome(String s) {
         if ("".equals(s)){
             return "";
         }
         String origin = s;
-        String reverse = new StringBuffer(s).reverse().toString();
+        //字符串反转
+        String reverse = new StringBuilder(s).reverse().toString();
+        int length = s.length();
+        int[][] arr = new int[length][length];
+        int maxLen = 0;
+        int maxEnd = 0;
+        for (int i = 0; i < length;i++){
+            for (int j=0;j < length;j++){
+                if (origin.charAt(i) == reverse.charAt(j)){
+                    if (i==0 || j==0){
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = arr[i -1][j - 1] + 1;
+                    }
+                }
+                if (arr[i][j] > maxLen){
+                    maxLen = arr[i][j];
+                    //以i位置
+                    maxEnd = i;
+                }
+            }
+        }
+        return s.substring(maxEnd - maxLen + 1,maxEnd + 1);
+    }
 
-        return null;
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("abc435cba"));
     }
 
 
