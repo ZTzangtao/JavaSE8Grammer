4d99c52e5136b0d39150a62615de57aaf8d29402
臧涛 <zangtao-sj@bestpay.com.cn>
2020-03-28 10:57:58 +0800

    narcissus algorithm

--- src/main/java/OwnTest/IntegerTest.java
+++ src/main/java/OwnTest/IntegerTest.java
@@ -9,20 +9,28 @@ package OwnTest;
 public class IntegerTest {
 
     public static void main(String[] args) {
-        int i = 355;
-        int a = (i % 100) / 10;
-        //或者
-        int b = (i / 10) % 10;
-        System.out.println("该数的十位是:" + a);
-        System.out.println("该数的十位是:" + b);
+//        int i = 355;
+//        int a = (i % 100) / 10;
+//        //或者
+//        int b = (i / 10) % 10;
+//        System.out.println("该数的十位是:" + a);
+//        System.out.println("该数的十位是:" + b);
+//
+//        Integer aa = 3;
+//        Integer bb = 3;
+//        Integer cc = 273;
+//        Integer dd = 273;
+//        System.out.println(aa.equals(bb));
+//        System.out.println(cc.equals(dd));
+//        System.out.println(aa == bb);
+//        System.out.println(cc == dd);
+        int i =1;
+        int j = i++;
+        if ((j> ++j) && (i++ == j)){
+            j+= i;
+        }
+        System.out.println(j);
 
-        Integer aa = 3;
-        Integer bb = 3;
-        Integer cc = 273;
-        Integer dd = 273;
-        System.out.println(aa.equals(bb));
-        System.out.println(cc.equals(dd));
-        System.out.println(aa == bb);
-        System.out.println(cc == dd);
+
     }
 }
