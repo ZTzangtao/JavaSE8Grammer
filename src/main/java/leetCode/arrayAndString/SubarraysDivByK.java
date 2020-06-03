62384030d0810ce75f2efbbf37658babefe9724d
zangtao <zangtao3211@163.com>
2020-06-03 21:06:25 +0800

    maxProduct

---
+++ src/main/java/leetCode/arrayAndString/SubarraysDivByK.java
@@ -0,0 +1,25 @@
+package leetCode.arrayAndString;
+
+/**
+ * @author zangtao
+ */
+public class SubarraysDivByK {
+
+    public static void main(String[] args) {
+
+        System.out.println(subarraysDivByK(new int[]{4,5,0,-2,-3,1},5));
+
+    }
+
+    /**
+     * 给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目。
+     *
+     * @param A 数组A
+     * @param K 被整除数K
+     * @return
+     */
+    public static int subarraysDivByK(int[] A, int K) {
+        return 0;
+
+    }
+}
