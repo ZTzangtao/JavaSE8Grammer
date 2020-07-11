346ef04e67e7e4dca27bf6bf3e2e62f12906a86e
zangtao <zangtao3211@163.com>
2020-07-11 22:31:10 +0800

    merge sort learn 07、11

--- src/main/java/classicalAlgorithm/MergeSort.java
+++ src/main/java/classicalAlgorithm/MergeSort.java
@@ -1,19 +1,26 @@
 package classicalAlgorithm;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
+    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(sort(new int[]{1,87,3,4,2,7,4,5,8,9,5,45,34,2,6,6,5,6,63,3,45345,3,5,6},0,9)));
+    }
+
     /** 归并排序主体函数 */
-    public void sort(int[] A, int lo,int hi){
+    public static int[] sort(int[] A, int lo, int hi){
         if (lo>=hi){
-            return;
+            return null;
         }
         int mid = lo + (hi - lo)/2;
         sort(A, lo, mid);
         sort(A , mid+1, hi);
         merge(A,lo,mid,hi);
+        return A;
     }
 
-    private void merge(int[] nums, int lo, int mid, int hi) {
+    private static void merge(int[] nums, int lo, int mid, int hi) {
         int[] copy =  nums.clone();
 
         int k = lo;
