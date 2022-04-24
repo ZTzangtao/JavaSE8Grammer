09d68c153748e9223ef5f50c8893676ff5cfc726
Tommy <zangtao3211@163.com>
2022-04-24 22:52:12 +0800

    jdk8 Stream

--- src/main/java/Stream/Java8StreamTest.java
+++ src/main/java/Stream/Java8StreamTest.java
@@ -1,5 +1,6 @@
 package Stream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,15 +13,37 @@ public class Java8StreamTest {
 
     public static void main(String[] args) {
         filter();
+        distinct();
     }
 
     /**
      * filter
+     *
+     * 过滤集合中不符合条件的元素
      */
     public static void filter(){
         List<String> strings = Arrays.asList("abc","","bc","efg","abcd","","jkl");
         List<String> filterList = strings.stream().filter(str -> str.contains("f")).collect(Collectors.toList());
         System.out.println(filterList);
+    }
+
+    /**
+     * distinct
+     *
+     * 返回一个元素各异（根据流所生成元素的hashCode和equals方法实现）的流
+     */
+    public static void distinct(){
+        List<String> strings = Arrays.asList("abc","","bc","efg","abcd","","jkl");
+        List<String> filterList = strings.stream().distinct().collect(Collectors.toList());
+        System.out.println(filterList);
+        //-------------------------------------------------------
+        List<User> users = new ArrayList<>();
+        users.add(new User(1,"z"));
+        users.add(new User(1,"z"));
+        users.add(new User(2,"s"));
+        List<User> usersList = users.stream().distinct().collect(Collectors.toList());
+        System.out.println(usersList);
+
     }
 
 }
---
+++ src/main/java/Stream/User.java
@@ -0,0 +1,17 @@
+package Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Author: Tommy
+ * @DATE: 2022/4/24
+ */
+@Data
+@AllArgsConstructor
+public class User {
+
+    private Integer id;
+    private String name;
+
+}
